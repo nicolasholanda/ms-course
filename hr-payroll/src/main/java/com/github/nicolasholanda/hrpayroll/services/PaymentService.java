b5a8c6bef4b7f85bdd9440b6e293051c83e4e221
nicolasholanda <nikolas.nhm14@gmail.com>
2023-07-17 23:44:43 -0300

    Comunicando projetos com RestTemplate

--- hr-payroll/src/main/java/com/github/nicolasholanda/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/github/nicolasholanda/hrpayroll/services/PaymentService.java
@@ -1,12 +1,30 @@
 package com.github.nicolasholanda.hrpayroll.services;
 
 import com.github.nicolasholanda.hrpayroll.entities.Payment;
+import com.github.nicolasholanda.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(long workerId, int days) {
-        return new Payment("Bob", 200.0, days);
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", String.valueOf(workerId));
+
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        assert worker != null;
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
