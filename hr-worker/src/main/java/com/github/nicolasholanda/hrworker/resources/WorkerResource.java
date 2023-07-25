873e891e664ec922eba1e84c59c9fb50a1112add
nicolasholanda <nikolas.nhm14@gmail.com>
2023-07-24 23:03:22 -0300

    Adicionando ribbon e fazendo downgrade do spring

--- hr-worker/src/main/java/com/github/nicolasholanda/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/github/nicolasholanda/hrworker/resources/WorkerResource.java
@@ -2,7 +2,10 @@ package com.github.nicolasholanda.hrworker.resources;
 
 import com.github.nicolasholanda.hrworker.entities.Worker;
 import com.github.nicolasholanda.hrworker.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +20,11 @@ import static org.springframework.http.ResponseEntity.ok;
 @RequestMapping("/workers")
 public class WorkerResource {
 
+    private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+    @Autowired
+    private Environment env;
+
     @Autowired
     private WorkerRepository repository;
 
@@ -27,6 +35,7 @@ public class WorkerResource {
 
     @GetMapping("{id}")
     public ResponseEntity<Worker> findById(@PathVariable("id") Long id) {
+        logger.info("Port = " + env.getProperty("local.server.port"));
         return ok(repository.findById(id).orElseGet(null));
     }
 }
