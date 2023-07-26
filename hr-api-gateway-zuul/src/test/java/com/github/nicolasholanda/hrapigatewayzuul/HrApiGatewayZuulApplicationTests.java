bc832c2d2501c7c48089ba8eea63a1185d49617f
nicolasholanda <nikolas.nhm14@gmail.com>
2023-07-25 21:22:29 -0300

    Adicionando projeto hr-api-gateway-zuul

---
+++ hr-api-gateway-zuul/src/test/java/com/github/nicolasholanda/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nicolasholanda.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
