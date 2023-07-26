a3db24021cf69e09f1b8c41d594644e2e908bb72
nicolasholanda <nikolas.nhm14@gmail.com>
2023-07-25 22:40:30 -0300

    Adicionando actuator

--- hr-worker/src/main/java/com/github/nicolasholanda/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/github/nicolasholanda/hrworker/resources/WorkerResource.java
@@ -5,6 +5,8 @@ import com.github.nicolasholanda.hrworker.repositories.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,17 +18,27 @@ import java.util.List;
 
 import static org.springframework.http.ResponseEntity.ok;
 
+@RefreshScope
 @RestController
 @RequestMapping("/workers")
 public class WorkerResource {
 
     private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
+    @Value("${test.config}")
+    private String testConfig;
+
     @Autowired
     private Environment env;
 
     @Autowired
     private WorkerRepository repository;
+
+    @GetMapping("/configs")
+    public ResponseEntity<Void> getConfig() {
+        logger.info("Config = " + testConfig);
+        return ResponseEntity.noContent().build();
+    }
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll() {
