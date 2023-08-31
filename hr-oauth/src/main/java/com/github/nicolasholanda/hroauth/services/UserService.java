8af201bcfc3c72eb32fe7182240825b87b2d3fff
nicolasholanda <nikolas.nhm14@gmail.com>
2023-08-30 23:34:20 -0300

    Login JWT token

--- hr-oauth/src/main/java/com/github/nicolasholanda/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/github/nicolasholanda/hroauth/services/UserService.java
@@ -3,18 +3,26 @@ package com.github.nicolasholanda.hroauth.services;
 import com.github.nicolasholanda.hroauth.entities.User;
 import com.github.nicolasholanda.hroauth.feignclients.UserFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     @Autowired
     private UserFeignClient userFeignClient;
 
     public User findByEmail(String email) {
         return Optional.ofNullable(userFeignClient.search(email).getBody())
-                .orElseThrow(() -> new IllegalArgumentException("Email not found"));
+                .orElseThrow(() -> new UsernameNotFoundException("Email not found"));
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return this.findByEmail(username);
     }
 }
